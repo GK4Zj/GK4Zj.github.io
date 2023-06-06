268867a3b653408f9b364976deaaeda7ee5e79b5
GK4Zj <107035536+GK4Zj@users.noreply.github.com>
2023-06-06 18:30:11 +0300

    Create lab2.js

---
+++ js/lab2/lab2.js
@@ -0,0 +1,54 @@
+function pow(x, n)
+{
+    return x**n; 
+}
+function sumTo(n)
+{
+    return (n*(n+1))/2 
+}
+function factorial(n)
+{
+    if (n<=0)
+    {
+        return 1n; 
+    }
+    else{
+        return ( BigInt (n) *  BigInt(factorial(n-1))); 
+    }
+}
+function fib(n){
+    a = BigInt(1); 
+    b=BigInt(1); 
+    if (n== BigInt(0))
+    {
+        return BigInt(0); 
+    }
+    else
+    {
+        for (i = BigInt(3); i<=n; i++) 
+        {
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
+function compare(x)
+{
+return function(y)
+{
+    if (y>x) return true;
+    else if (y<x) return false;
+    else if (y==x) return null;
+}
+}
+function sum()
+{
+    var result=0; //сумма
+    for (var i=0; i<arguments.length; i++)
+    {
+        result+=arguments[i]
+    }
+    return result;
+}
